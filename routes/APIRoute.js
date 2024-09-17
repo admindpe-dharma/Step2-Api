bbe567720b9b544aee23bab9078e9082f4cbe2bf
Franstio <franstiodiego01@gmail.com>
2024-09-17 15:19:25 +0700

    ok

--- routes/APIRoute.js
+++ routes/APIRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { syncTransaction, TransactionStep1 } from '../controllers/Employee.js';
+import { getIp } from '../controllers/Bin.js';
 
 const routes = express.Router();
 
 routes.post('/Step1',TransactionStep1);
 routes.get("/Sync",syncTransaction);
+routes.get('/ip',getIp);
 export default routes;
