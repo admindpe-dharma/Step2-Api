0bbebc1d653e63e8191d9482e2aceb8431cf9217
Franstio <franstiodiego01@gmail.com>
2024-09-20 18:51:48 +0700

    SUPPORT OFFLINE

--- controllers/Employee.js
+++ controllers/Employee.js
@@ -347,6 +347,7 @@ export const syncPendingTransaction = async ()=>{
         console.log(transactionPending[i]);
         const statuses = transactionPending[i].status.split('|');
         statuses.splice(statuses.indexOf('PENDING'),1);
+        statuses.splice(statuses.indexOf('Pending'),1);
         if (statuses.includes("PIDSG"))
         {
             await axios.get(`http://${process.env.PIDSG}/api/pid/pibadgeverify?f1=${transactionPending[i].station}&f2=${transactionPending[i].badgeId}`,{validateStatus: (s)=>true});
