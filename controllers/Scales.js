e1069ce7b56e592e1e4f7cf264a438704476d470
Franstio <franstiodiego01@gmail.com>
2024-12-16 13:55:18 +0700

    ok

--- controllers/Scales.js
+++ controllers/Scales.js
@@ -65,7 +65,7 @@ const processWeight = async (payload) =>{
         return true;
     }
     else if (match4 && match4.length && match4.length > 0) {
-        const weight = match[1];
+        const weight = match4[1];
         //response = { weight: parseFloat(weight) };
         const response = { weight50Kg: weight };
         io.emit('data1', response);
